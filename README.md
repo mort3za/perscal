decf93224573a25a867c064b9bea44a0a4f51ef9
Morteza <morteza@pepper.com>
2023-03-18 18:29:09 +0100

    Add main functionalities, add readme

---
+++ README.md
@@ -0,0 +1,54 @@
+# PersCal - Persian-Gregorian Calendar Converter
+
+This is a command-line interface (CLI) tool for converting between Gregorian and Persian calendars. The tool is built with Node.js and utilizes the `jalaali-js` package for the calendar conversion.
+
+## Installation
+
+To install the tool, first, you need to have Node.js installed on your machine. Then you can install the package via npm:
+
+`npm install -g perscal`
+
+## Usage
+
+The tool provides several options for converting between the calendars. You can use the `--help` option to see the available options:
+
+`perscal --help`
+
+### Converting Gregorian to Persian
+
+You can convert a Gregorian date to Persian using one of the following options:
+
+- `-t, --timestamp <seconds>`: Unix timestamp (seconds since 1970-01-01 00:00:00 UTC)
+- `-dt, --datetime <datetime>`: Gregorian datetime (YYYY-MM-DD HH:mm) or other standard formats like ISO 8601
+- `-y, --year <year>`: Gregorian year
+- `-m, --month <month>`: Gregorian month
+- `-d, --day <day>`: Gregorian day
+
+Example usage:
+
+`perscal --year 2022 --month 3 --day 18 perscal --datetime "2022-03-18 09:30"`
+
+### Converting Persian to Gregorian
+
+You can convert a Persian date to Gregorian using one of the following options:
+
+- `-pd, --persian-date <persian-date>`: Persian date (YYYY-MM-DD)
+- `-py, --persian-year <persian-year>`: Persian year
+- `-pm, --persian-month <persian-month>`: Persian month
+- `-pdy, --persian-day <persian-day>`: Persian day
+
+Example usage:
+
+`perscal --persian-date "1401-12-28" perscal --persian-year 1401 --persian-month 12 --persian-day 28`
+
+### Output Separator for Persian Date
+
+You can use the `-s, --separator <char>` option to specify the separator character for the output Persian date. The default separator is `-`.
+
+Example usage:
+
+`perscal --year 2022 --month 3 --day 18 --separator /`
+
+## License
+
+This tool is licensed under the MIT license. See the [LICENSE](LICENSE) file for details.
--- index.js
+++ index.js
@@ -1,6 +1,101 @@
 #! /usr/bin/env node
-var jalaali = require("jalaali-js");
 
-const jd = jalaali.toJalaali(new Date());
-const jds = jd.jy + "-" + jd.jm + "-" + jd.jd;
-console.log(jds);
+const { program } = require("commander");
+const jalaali = require("jalaali-js");
+
+program
+  .option("-t, --timestamp <seconds>", "Unix timestamp (seconds since 1970-01-01 00:00:00 UTC)")
+  .option("-dt, --datetime <datetime>", "Gregorian datetime (YYYY-MM-DD HH:mm) or other standard formats like ISO 8601")
+  .option("-y, --year <year>", "Gregorian year")
+  .option("-m, --month <month>", "Gregorian month")
+  .option("-d, --day <day>", "Gregorian day")
+  .option("-pd, --persian-date <persian-date>", "Persian date (YYYY-MM-DD)")
+  .option("-py, --persian-year <persian-year>", "Persian year")
+  .option("-pm, --persian-month <persian-month>", "Persian month")
+  .option("-pdy, --persian-day <persian-day>", "Persian day")
+  .option("-s, --separator <char>", "Output separator for Persian date", "-")
+  .option("-h, --help", "Show help")
+  .name("perscal")
+  .description("CLI tool for converting between Gregorian and Persian calendars")
+  .version("1.0.3");
+
+program.parse();
+const options = program.opts();
+
+function exit(message, code = 0) {
+  console.log(message);
+  process.exit(code);
+}
+
+function toPersian({ timestamp, datetime, year, month = "01", day = "01" }) {
+  if (timestamp) {
+    return jalaali.toJalaali(new Date(timestamp * 1000));
+  }
+  if (datetime) {
+    return jalaali.toJalaali(new Date(datetime));
+  }
+  if (year) {
+    const yyyy = year.padStart(4, "0");
+    const mm = month.padStart(2, "0");
+    const dd = day.padStart(2, "0");
+    const dateString = `${yyyy}-${mm}-${dd}`;
+    return jalaali.toJalaali(new Date(dateString));
+  }
+
+  exit("No input date provided");
+}
+function toGreg({ persianDate, persianYear, persianMonth = 1, persianDay = 0 }) {
+  if (persianDate) {
+    const pd = persianDate.split("-").map((i) => parseInt(i));
+    return jalaali.toGregorian(pd[0], pd[1], pd[2]);
+  }
+  return jalaali.toGregorian(parseInt(persianYear), parseInt(persianMonth), parseInt(persianDay));
+}
+
+const PERSIAN_PARAMS = ["persianDate", "persianYear", "persianMonth", "persianDay"];
+const GREG_PARAMS = ["timestamp", "datetime", "year", "month", "day"];
+
+const isInputDatePersian = PERSIAN_PARAMS.some((param) => options[param]);
+const isInputDateGregorian = GREG_PARAMS.some((param) => options[param]);
+
+function printPersianDate(jdObject) {
+  pdString = `${jdObject.jy}${options.separator}${jdObject.jm}${options.separator}${jdObject.jd}`;
+  console.log(pdString);
+}
+
+function printGregorianDate(dObject) {
+  const gdString = `${dObject.gy}-${dObject.gm}-${dObject.gd}`;
+  console.log(gdString);
+}
+
+function main() {
+  if (isInputDatePersian) {
+    const converted = toGreg({
+      persianDate: options["persianDate"],
+      persianYear: options["persianYear"],
+      persianMonth: options["persianMonth"],
+      persianDay: options["persianDay"],
+    });
+    printGregorianDate(converted);
+  } else if (isInputDateGregorian) {
+    const converted = toPersian({
+      timestamp: options.timestamp,
+      datetime: options.datetime,
+      year: options.year,
+      month: options.month,
+      day: options.day,
+    });
+    printPersianDate(converted);
+  } else if (options.help) {
+    program.help();
+  } else {
+    const jdObject = toPersian({ timestamp: Date.now() / 1000 });
+    printPersianDate(jdObject);
+  }
+}
+
+try {
+  main();
+} catch (error) {
+  exit("Invalid options provided, use --help for more information.");
+}
